35af357b02b4f50de0b64c45e5cde39f6d20adc4
Promise Tshegofatso magoga <104593732+Promisemagoga@users.noreply.github.com>
2023-01-27 17:45:13 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,30 @@
+var buttons = document.querySelectorAll('button')
+var display = document.getElementById('display')
+var operator = ''
+var operand1 = ''
+var operand2 = ''
+
+for(var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function(){
+        var value = this.innerHTML
+        if(value >= 0 || value <= 9){
+            if (operator == ''){
+                operand1 += value
+            }else{
+                operand2 += value
+            }
+            display.value = operand1 + operator + operand2
+        }else if( value == '+' || value == '-' || value == '*' || value == '/'){
+            operator = value
+            display.value = operand1 + operator + operand2
+        }else if(value == 'C'){
+            operator = ''
+            operand1= ''
+            operand2 = ''
+            display.value = ''
+        }else if (value == '='){
+            var result = eval(operand1 + operator + operand2)
+            display.value = result
+        }
+    })
+}
